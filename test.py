6c167a93729cdf5c94972c0679b55a936f03cec5
rhnikjk <mccroner@wsc.school.nz>
2024-06-08 21:06:14 +1200

    Update test.py

--- test.py
+++ test.py
@@ -2,30 +2,12 @@ EVENTS = [["ma"],["la"]]
 ACTIONS = [[["f","e","a"],[0,1,1]],[["s","i"], [0,1]]]
 CONSIQUENCES = []
 INVENTORY_FORMAT = "{}"
-equipped_items = [["cannnon",1],["basic armor",2]]
-inventory_items = [["sword",1],["mediam armor",2],["bow",1],["gun",1]]
+equipped_items = [["cannnon",1,11],["basic armor",2,20]]
+inventory_items = [["sword",1,5],["mediam armor",2,25],["bow",1,7],["gun",1,10]]
 ALL_ITEMS = ["cannon", "basic armor", "sword", "knife", "bow", "gun"]
+ENEMYS = [["scout",10,3],["warrior",15,5],["tank",25,4]]
 
-def equip(index_value,i):
-    print("do you want to remove", equipped_items[index_value][0])
-    user = input("yes/no\n").lower()
-    if user == "yes" or user == "y":
-        inventory_items.append(equipped_items[index_value])
-        equipped_items.remove(equipped_items[index_value])
-        equipped_items.append(inventory_items[i])
-    elif user == "no" or user == "n":
-        print("ok")
-    else:
-        print("thats not a command")
-
-def print_inventory():
-    print("\nin inventory:")
-    for i in range(len(inventory_items)):
-        print(INVENTORY_FORMAT.format(inventory_items[i][0]))
-    print("\nequipped:")
-    for i in range(len(equipped_items)):
-        print(equipped_items[i][0])
-
+#-----------------------FUNCTIONS--------------------------
 def story_loop():
     path = 0
     x=0
@@ -48,7 +30,17 @@ def story_loop():
         else:
             print("thats not an action")
             y=0
-           
+
+def health():
+    health = equipped_items[1][2]
+
+def combat(enemytype):
+    print(enemytype, "attacks")
+    user = input("Fight\nitems\nflee").lower
+    if user == "fight":
+        print("")
+
+#-------------------INVENTORY FUNCTIONS---------------------
 
 def inventory():
     print_inventory()     
@@ -74,5 +66,29 @@ def inventory():
                     break
                 else:
                     print("thats not a command")
+
+
+def equip(index_value,i):
+    print("do you want to swap for", equipped_items[index_value][0])
+    user = input("yes/no\n").lower()
+    if user == "yes" or user == "y":
+        inventory_items.append(equipped_items[index_value])
+        equipped_items.remove(equipped_items[index_value])
+        equipped_items.append(inventory_items[i])
+    elif user == "no" or user == "n":
+        print("ok")
+    else:
+        print("thats not a command")
+
+def print_inventory():
+    print("\nin inventory:")
+    for i in range(len(inventory_items)):
+        print(INVENTORY_FORMAT.format(inventory_items[i][0]))
+    print("\nequipped:")
+    for i in range(len(equipped_items)):
+        print(equipped_items[i][0])
+
+
+
 
 story_loop()
