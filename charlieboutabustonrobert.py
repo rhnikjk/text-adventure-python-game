1bff7cbffa6dd5fab59b1daea4769aa43b1fdedb
charcharyas <hillc@wsc.school.nz>
2024-06-14 11:37:36 +1200

    Update charlieboutabustonrobert.py

--- charlieboutabustonrobert.py
+++ charlieboutabustonrobert.py
@@ -2,6 +2,17 @@ def intro():
     name= ("Robat")
     input("whats your name" )
     print("I noticed you made a spelling mistake but fair not I can see you were writing Robat :D")
-    input("HEY",name, "welcome to (insert game name here) ")
+    ready = input("HEY",name, "welcome to ---- Are you ready to play? y/n")
+    if ready == ("yes"):
+        print ("lets jump into it")
+    else:
+        while True:
+        
+            readyn =input("well {name}, are you ready now? ")
+            readyn = ("yes")
+            break
+        except :
+            print("oh my")
+            
 
 intro()
