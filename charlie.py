150582c020f52536eb9e66522932ba1985e5f865
charcharyas <hillc@wsc.school.nz>
2024-07-02 12:26:22 +1200

    updated intro

--- charlie.py
+++ charlie.py
@@ -1,7 +1,9 @@
 def intro():
     input("whats your name\n" )
     print("I noticed you made a spelling mistake but fair not I can see you were writing Robat :D")
-    ready = input( "welcome Robat to ---- Are you ready to play? yes or no\n")
+    print("Hey Robat and welcome to our game")
+    print("c")
+    ready = input( "Are you ready to play? yes or no\n")
     while True:
         if ready == ("yes"):
             print ("lets jump into it")
